477bc4506530867485b664c0d7d36711d68547f0
jai <vjaivasanth@gmail.com>
2026-01-23 15:42:45 +0800

    Created Ui.java, Protagonist Java

---
+++ src/main/java/Protagonist.java
@@ -0,0 +1,9 @@
+public class Protagonist {
+    public static void main(String[] args) {
+        Ui.line();
+        Ui.greeting();
+        Ui.line();
+        Ui.goodbye();
+        Ui.line();
+    }
+}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,14 @@
+public class Ui {
+    public static void greeting() {
+        System.out.println("Greetings from The Protagonist\nHow may I assist you?");
+    }
+
+    public static void line() {
+        System.out.println("________________________________________");
+    }
+
+    public static void goodbye() {
+        System.out.println("Goodbye. May our paths cross again.");
+    }
+
+}
