c455dc6d916cc883be68166b80028ec1860de205
jai <vjaivasanth@gmail.com>
2026-02-05 23:03:19 +0800

    Update .gitignore

--- src/test/java/protagonist/task/TaskListTest.java
+++ src/test/java/protagonist/task/TaskListTest.java
@@ -10,10 +10,10 @@ public class TaskListTest {
     public void add_increasesSize() {
         TaskList tasks = new TaskList();
 
-        tasks.add(new ToDo("read book"));
+        tasks.addTask(new ToDo("read book"));
         assertEquals(1, tasks.size());
 
-        tasks.add(new ToDo("drink water"));
+        tasks.addTask(new ToDo("drink water"));
         assertEquals(2, tasks.size());
     }
 
@@ -21,8 +21,8 @@ public class TaskListTest {
     public void remove_decreasesSize() {
         TaskList tasks = new TaskList();
 
-        tasks.add(new ToDo("A"));
-        tasks.add(new ToDo("B"));
+        tasks.addTask(new ToDo("A"));
+        tasks.addTask(new ToDo("B"));
         assertEquals(2, tasks.size());
 
         tasks.removeTask(0);
