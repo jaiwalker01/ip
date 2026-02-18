2f378299b5c32cfbb934d09d77e1dd4c5542e7aa
jai <vjaivasanth@gmail.com>
2026-02-18 13:29:41 +0800

    Update GUI window title

--- src/main/java/protagonist/gui/Main.java
+++ src/main/java/protagonist/gui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);  // inject the Duke instance
+            stage.setTitle("Protagonist");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
